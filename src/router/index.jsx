9b3eb9ab207d078bbeede98b660fa6d053fe738e
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-06-12 15:24:21 +0200

    creado selector

---
+++ src/router/index.jsx
@@ -0,0 +1,14 @@
+import Earbuds from "../pages/Earbuds";
+import { createBrowserRouter } from "react-router-dom";
+export const router = createBrowserRouter([
+  {
+    // path: "/",
+    // element: <Layout />,
+    // children: [
+    //   {
+    path: "/",
+    element: <Earbuds />,
+    //   },
+    // ],
+  },
+]);
